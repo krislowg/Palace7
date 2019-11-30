872a6eb87ab9f2b6550f10b213fe7cc2ddabc020
Mireya-Hernandez <42775432+Mireya-Hernandez@users.noreply.github.com>
2019-11-30 14:39:58 -0500

    Changed Confirmation Message

--- src/main/java/ResortArenaPalace/BarChartEx.java
+++ src/main/java/ResortArenaPalace/BarChartEx.java
@@ -1,5 +1,6 @@
 package ResortArenaPalace;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,12 +8,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
+import javafx.scene.control.SortEvent;
+import javafx.stage.Stage;
 
 public class BarChartEx {
 
